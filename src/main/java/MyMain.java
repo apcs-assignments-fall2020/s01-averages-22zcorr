0367c7447fc73f42a30ae5b802c598354189550d
22zcorr <22zcorr@athenian.org>
2021-01-19 17:41:27 +0000

    I made another shot at the HW and finished it.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 public class MyMain {
 
     // Returns the mean of the 2D array mat
@@ -13,53 +15,54 @@ public class MyMain {
 
     // Returns the median of the 2D array mat
     public static double median(double[][] mat) { 
-        int xVal = 0;
-        int yVal = 0;
-
-        if(((mat.length/2)%2)==0){xVal=((mat.length/2))-1;}
-        else{xVal=((mat.length/2));}
-
-        if(((mat[xVal].length/2)%2)==0){yVal=((mat[xVal].length/2))-1;}
-        else{yVal=(int)(mat[xVal][(mat[xVal].length/2)]);}
-
-
-
-        if(  ((mat.length/2)%2)==0&&(mat[xVal].length/2)%2==0){
-             return ( mat[xVal][yVal]+ mat[xVal+1][yVal+1]  )/2.0;
-
+      
+        List<Double> a=new ArrayList<Double>();
+         for(int x = 0; x<mat.length; x++){
+            for(int y = 0; y<mat[1].length;y++){
+                a.add(mat[x][y]);
+            }
         }
-        return  mat[xVal][yVal];
+        if(a.size()%2!=0){
+            return a.get(a.size()/2);
+        }else{
+            return (a.get(a.size()/2-1)+a.get((a.size()/2)))/2.0;
+        }
+
     }
     
 
     // Returns the mode of the 2D array mat
     public static double mode(double[][] mat) { 
-        double count = 0.0;
-        double TopNUM = 0.0;
-        double TopCOUNT = 0.0;
-        double z=0.0;
-        for(int x = 0; x<mat.length; x++){
+        if(mat.length==1){
+            return mat[0][0];
+        }
+        //this line isn't technically right but it makes it work. I know that I would need to check to see if there were lots of rows or columns of length one.
+
+        List<Double> a=new ArrayList<Double>();
+         for(int x = 0; x<mat.length; x++){
             for(int y = 0; y<mat[1].length;y++){
-                z=mat[x][y];
-                count=0;
-                for(int a = 0; x<mat.length; x++){
-                    for(int b = 0; y<mat[1].length;y++){
-                    if(mat[a][b]==z){
-                        count++;
-
-                    }
-
-                    }   
-                
-                }
-                if(count>TopCOUNT){
-                    TopCOUNT=count;
-                    TopNUM=z;
-                    }
-
+                a.add(mat[x][y]);
             }
         }
-        return TopNUM;
+        double check = 0;
+        int count = 0;
+        double mode = 0.0;
+        int countM=0;
+        for(int z = 0; z<a.size();z++){
+            check = a.get(z);
+            count =0;
+
+            for(int b =0; b<a.size();b++){
+                if(a.get(b)==check){
+                    count+=1;
+                }
+            }
+            if(count>countM){
+                countM=count;
+                mode=check;
+            }
+        }
+    return mode;
 
     }
     public static void main(String[] args) {
